b6b32ccb726275ae2a739cc520150925ea3485a4
zaurqurbanov <qurbanovzaur078@gmail.com>
2024-10-10 22:54:16 +0400

    change HasRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import { PersistGate } from 'redux-persist/integration/react'
@@ -12,9 +12,9 @@ createRoot(document.getElementById('root')).render(
   <Provider store={store}>
   
 
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>
+  </Router>
 
   
   </Provider>
